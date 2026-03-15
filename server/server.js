6e4df6933c4a059f699cb57d8b92db2ed4758e1e
dashdondog <157295419+dashdondog@users.noreply.github.com>
2026-03-16 01:24:10 +0800

    Correct CORS origin to 3000

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-  "http://localhost:8500",
+  "http://localhost:3000",
   "https://news-two-jade-76.vercel.app", // Энд өөрийн Vercel хаягийг нэмээрэй
 ];
 
